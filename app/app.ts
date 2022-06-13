2aa94c20d635be90c238f80c8afb8201d96717fd
Alex <alex_cubas1.0@hotmail.com>
2022-06-13 14:12:27 -0300

    userAddress-back-listen-app-difinitions

---
+++ app/app.ts
@@ -0,0 +1,30 @@
+import * as express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.config();
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.warn('Listen on ', PORT));
+  }
+}
+
+export { App }
+
+export const { app } = new App();
